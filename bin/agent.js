b2858377d362eb7ecad3e254c5d2db7d8489debb
David Sobreira Gouvea <davidsgbr@gmail.com>
2022-01-03 00:38:25 -0800

    add fan speed

--- bin/agent.js
+++ bin/agent.js
@@ -1,8 +1,10 @@
-const { listGpus, updateTemperature } = require("../lib/Gpu");
+const { listGpus, updateTemperature, updateFanSpeed } = require("../lib/Gpu");
+
+const gpus = listGpus();
 
 setInterval(() => {
-  const gpus = listGpus();
   updateTemperature(gpus);
+  updateFanSpeed(gpus);
 
   console.dir(gpus, { depth: 5 });
 }, 10_000);
