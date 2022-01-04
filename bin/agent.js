aff077a1efe38b0a6ee30855aa963a9836b3ed17
David Sobreira Gouvea <davidsgbr@gmail.com>
2022-01-04 02:28:06 -0800

    analyze temperature

--- bin/agent.js
+++ bin/agent.js
@@ -1,10 +1,17 @@
-const { listGpus, updateTemperature, updateFanSpeed } = require("../lib/Gpu");
+const { listGpus, readPowerTemperature, readFanSpeed } = require("../lib/Gpu");
+const { analyzeTemperature } = require("../lib/TemperatureAnalyzer");
+
+const DEFAULT_INTERVAL = 30_000;
+const interval = DEFAULT_INTERVAL;
+
+const DEFAULT_TEMPERATURE = 60;
+const temperature = DEFAULT_TEMPERATURE;
 
 const gpus = listGpus();
 
 setInterval(() => {
-  updateTemperature(gpus);
-  updateFanSpeed(gpus);
+  readPowerTemperature(gpus);
+  readFanSpeed(gpus);
 
-  console.dir(gpus, { depth: 5 });
-}, 10_000);
+  analyzeTemperature(gpus, temperature);
+}, interval);
