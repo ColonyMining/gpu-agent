b5513c0686a7ff55ac349b3ca15b6736ec10d485
David Sobreira Gouvea <davidsgbr@gmail.com>
2022-01-05 02:13:58 -0800

    create config

--- bin/list.js
+++ bin/list.js
@@ -6,4 +6,14 @@ readPowerTemperature(gpus);
 readFanSpeed(gpus);
 readOverclock(gpus);
 
+console.log("###################################");
+console.log("#########      GPUS      ##########");
+console.log("###################################");
+
 console.dir(gpus, { depth: 5 });
+
+console.log("###################################");
+console.log("#########      CONFIG      ########");
+console.log("###################################");
+
+console.dir(gpus.map(gpuToConfig), { depth: 5 });
