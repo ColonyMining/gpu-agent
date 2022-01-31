a7072da0d51b1b2134f48f72a13d9e88ed4abe26
David Sobreira Gouvea <davidsgbr@gmail.com>
2022-01-31 01:43:48 -0800

    use error handler

--- test.js
+++ test.js
@@ -5,7 +5,9 @@ const { v4: uuid } = require("uuid");
 const tmpFile = `/tmp/${uuid()}.out`;
 const stream = fs.createWriteStream(tmpFile);
 
-childProcess.execSync("nvidia-settings -c :0 -q gpus", { stdio: [stream, stream, stream] });
+stream.on("open", () => {
+  childProcess.execSync("nvidia-settings -c :0 -q gpus", { stdio: [stream, stream, stream] });
+});
 
 response = fs.readFileSync(tmpFile).toString("utf-8");
 
