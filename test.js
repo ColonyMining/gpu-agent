351ebbfaeae83870cf63329685efb846b1652154
David Sobreira Gouvea <davidsgbr@gmail.com>
2022-01-31 01:38:40 -0800

    use error handler

--- test.js
+++ test.js
@@ -1,15 +1,12 @@
-async function test() {
-  return new Promise((resolve, reject) => {
-    resolve("cool");
-  });
-}
+const childProcess = require("child_process");
+const fs = require("fs");
+const { v4: uuid } = require("uuid");
 
-function sync() {
-  let output;
-  Promise.all([test()]);
-  return output;
-}
+const tmpFile = `/tmp/${uuid()}.out`;
+const stream = fs.createWriteStream(tmpFile);
 
-console.log("a");
-console.log(sync());
-console.log("b");
+childProcess.execSync("nvidia-settings -c :0 -q gpus", { stdio: [stream, stream, stream] });
+
+response = fs.readFileSync(tmpFile).toString("utf-8");
+
+console.log(response);
